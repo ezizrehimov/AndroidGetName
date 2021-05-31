d3a4a63e07188384aac285492bdbdb874b1eb29f
Eziz Rehimov <fantastig@bk.ru>
2021-05-31 13:37:55 +0400

    first project completed - View My Name

--- app/src/main/java/com/eziz/helloworld/MainActivity.kt
+++ app/src/main/java/com/eziz/helloworld/MainActivity.kt
@@ -2,10 +2,20 @@ package com.eziz.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.button)
+        val seeText: TextView = findViewById(R.id.seeText)
+        val addName: EditText = findViewById(R.id.addName)
+        button.setOnClickListener {
+            seeText.text = addName.text
+            addName.text.clear()
+        }
     }
 }
